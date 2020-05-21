80fbfec58fd405bffd2e4809c015d57ac4adbf07
皮卡丘打西瓜汁 <s0610078@my.cmsh.cyc.edu.tw>
2020-05-21 12:54:15 +0800

    Add files via upload

---
+++ 011-五則運算(尚未解決).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(int argc, char** argv)
+{
+	string input;
+	cin >> input;
+	int ¥ª¬A¸¹, ¥k¬A¸¹;
+	for (int i = 0; i < input.length(); i++)
+	{
+	}
+	system("PAUSE");
+	return 0;
+}
