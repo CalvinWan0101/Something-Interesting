050d2aa641c6fa1978af0fef6db0fc9a64eeb2cf
意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2021-02-26 12:15:26 +0800

    Update 計時器.cpp

--- 計時器.cpp
+++ 計時器.cpp
@@ -1,31 +1,31 @@
 #include <iostream>
-#include<iomanip>
-#include<ctime>
+#include <iomanip>
+#include <ctime>
 using namespace std;
-int main(int argc, char** argv)
+int main()
 {
-	cout.setf(ios::fixed | ios::right);
-	cout.fill('0');
-	//使用者直接輸入當前時間
-	int a, b, c;
-	cout << "請分別輸入當前的小時數，分鐘數，秒數" << endl;
-	cin >> a >> b >> c;
-	while (true)
-	{
-		for (int hr = a; hr < 24; hr++)
-		{
-			for (int min = b; min < 59; min++)
-			{
-				for (int sec = c; sec < 59; sec++)
-				{
-					cout << setw(2) << hr << ':' << setw(2) <<min<< ':' << setw(2) << sec << ' ';
-					cout << '\r';
-					unsigned int StartTime = time(NULL) + 1;
-					while (time(0) < StartTime);
-				}
-			}
-		}
-	}
-	system("PAUSE");
-	return 0;
+    cout.setf(ios::fixed | ios::right);
+    cout.fill('0');
+    int a, b, c;
+    //input the time start
+    cout << "Time: ";
+    cin >> a >> b >> c;
+    while (true)
+    {
+        for (int hr = a; hr < 24; hr++)
+        {
+            for (int min = b; min < 59; min++)
+            {
+                for (int sec = c; sec < 59; sec++)
+                {
+                    cout << setw(2) << hr << ':' << setw(2) << min << ':' << setw(2) << sec << ' ';
+                    cout << '\r';
+                    unsigned int StartTime = time(NULL) + 1;
+                    while (time(0) < StartTime)
+                        ;
+                }
+            }
+        }
+    }
+    return 0;
 }
