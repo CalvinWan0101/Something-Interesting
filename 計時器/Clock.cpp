64ac513446f186fb6b371a0a064b2ee71972e8d3
意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2021-03-12 16:10:15 +0800

    Update and rename 計時器.cpp to 計時器/Clock.cpp

--- 計時器/Clock.cpp
+++ 計時器/Clock.cpp
@@ -7,25 +7,17 @@ int main()
     cout.setf(ios::fixed | ios::right);
     cout.fill('0');
     int a, b, c;
-    //input the time start
     cout << "Time: ";
     cin >> a >> b >> c;
-    while (true)
-    {
+    while ()
         for (int hr = a; hr < 24; hr++)
-        {
             for (int min = b; min < 59; min++)
-            {
                 for (int sec = c; sec < 59; sec++)
                 {
                     cout << setw(2) << hr << ':' << setw(2) << min << ':' << setw(2) << sec << ' ';
                     cout << '\r';
                     unsigned int StartTime = time(NULL) + 1;
-                    while (time(0) < StartTime)
-                        ;
+                    while (time(0) < StartTime);
                 }
-            }
-        }
-    }
     return 0;
 }
