c33432cb875609751509a7a6173bfc5c0e97d3ab
意大利麵的最後希望 <s0610078@my.cmsh.cyc.edu.tw>
2021-03-16 10:29:38 +0800

    Update Basic(1.0).cpp

--- 走迷宮/Basic(1.0).cpp
+++ 走迷宮/Basic(1.0).cpp
@@ -1,14 +1,15 @@
+
 #include <iostream>
 #include <windows.h>
 #include <conio.h>
-char a[50][50] = {"############",
-                  "#O#    #   #",
-                  "#   ## # # #",
-                  "#####    # #",
-                  "#     #### #",
-                  "# #####  # #",
-                  "#       ##  ",
-                  "############"};
+char map[50][50] = {"############",
+                    "#O#    #   #",
+                    "#   ## # # #",
+                    "#####    # #",
+                    "#     #### #",
+                    "# #####  # #",
+                    "#       ##  ",
+                    "############"};
 using namespace std;
 int main()
 {
@@ -17,7 +18,7 @@ int main()
     for (int i = 0; i <= 7; i++)
     {
         for (int j = 0; j <= 11; j++)
-            cout << a[i][j];
+            cout << map[i][j];
         cout << endl;
     }
     while (true)
@@ -25,39 +26,39 @@ int main()
         //input no enter
         input = getch();
         //down
-        if (input == 's' && a[x + 1][y] == ' ')
+        if (input == 's' && map[x + 1][y] == ' ')
         {
-            a[x][y] = ' ';
+            map[x][y] = ' ';
             x++;
-            a[x][y] = 'O';
+            map[x][y] = 'O';
         }
         //up
-        else if (input == 'w' && a[x - 1][y] == ' ')
+        else if (input == 'w' && map[x - 1][y] == ' ')
         {
-            a[x][y] = ' ';
+            map[x][y] = ' ';
             x--;
-            a[x][y] = 'O';
+            map[x][y] = 'O';
         }
         //left
-        else if (input == 'a' && a[x][y - 1] == ' ')
+        else if (input == 'a' && map[x][y - 1] == ' ')
         {
-            a[x][y] = ' ';
+            map[x][y] = ' ';
             y--;
-            a[x][y] = 'O';
+            map[x][y] = 'O';
         }
         //right
-        else if (input == 'd' && a[x][y + 1] == ' ')
+        else if (input == 'd' && map[x][y + 1] == ' ')
         {
-            a[x][y] = ' ';
+            map[x][y] = ' ';
             y++;
-            a[x][y] = 'O';
+            map[x][y] = 'O';
         }
         //clear screen
         system("cls");
         for (int i = 0; i <= 7; i++)
         {
             for (int j = 0; j <= 11; j++)
-                cout << a[i][j];
+                cout << map[i][j];
             cout << endl;
         }
         if (x == 6 && y == 11)
